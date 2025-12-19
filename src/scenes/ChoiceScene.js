1e95051a08db5b4563d6c6e75a5d5c4762525ec7
alexisf <alexis.fabien45@gmail.com>
2025-12-19 11:03:03 +0100

    ajout de ChoiceScene

---
+++ src/scenes/ChoiceScene.js
@@ -0,0 +1,18 @@
+import { Player } from "../entities/player"
+
+const stateSelect = {
+    null : 0,
+    p1 : 1,
+    p2 : 2,
+
+}
+const selectPlayer = {
+    id : 'red',
+    skin : 'RedProtector',
+    Image : img.RedProtector,
+    state : stateSelect.null,
+}
+
+if (stateSelect === stateSelect.null) {
+    
+}
